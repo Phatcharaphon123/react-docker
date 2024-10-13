2c3341554ab3903b8d28e5353b5d7dd04a441fcf
Phatcharaphon yodrach <151843443+Phatcharaphon123@users.noreply.github.com>
2024-10-13 17:29:51 +0700

    add bt v.0.3

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -27,6 +27,7 @@ export default function MyCard(props) {
   </CardContent>
   <CardActions>
     <Button size="small">xxx</Button>
+    <Button size="small">my bt</Button>
     <Button size="small">Learn More</Button>
   </CardActions>
 </Card>
